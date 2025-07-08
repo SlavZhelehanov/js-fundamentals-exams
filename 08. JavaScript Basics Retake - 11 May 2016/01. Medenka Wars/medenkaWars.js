4f6c81a80db5952025af08e077704b3a3afb17b3
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-07-08 12:13:26 +0300

    Add solution to task 1: Medenka Wars

---
+++ 08. JavaScript Basics Retake - 11 May 2016/01. Medenka Wars/medenkaWars.js
@@ -0,0 +1,46 @@
+function  medenkaWars(strings) {
+    let Naskor = { consecutive: 0, tmpDmg: 0, total: 0 }, Vitkor = { consecutive: 0, tmpDmg: 0, total: 0 };
+
+    for (let i = 0; i < strings.length; i++) {
+        let [damage, attacker, _] =  strings[i].split(" ");
+
+        if (attacker === "dark") {
+            if (Naskor.consecutive === 0) {
+                Naskor.consecutive++;
+                Naskor.tmpDmg = +damage;
+                Naskor.total += +damage;
+            } else if (Naskor.consecutive < 4 && Naskor.tmpDmg === +damage) {
+                Naskor.consecutive++;
+                Naskor.total += +damage;
+            } else if (Naskor.consecutive === 4 && Naskor.tmpDmg === +damage) {
+                Naskor.consecutive = 0;
+                Naskor.tmpDmg = +damage;
+                Naskor.total += (damage * 4.5);
+            } else {
+                Naskor.consecutive = 1;
+                Naskor.tmpDmg = +damage;
+                Naskor.total += +damage;
+            }
+        } else {
+            if (Vitkor.consecutive === 0) {
+                Vitkor.consecutive++;
+                Vitkor.tmpDmg = +damage;
+                Vitkor.total += +damage;
+            } else if (Vitkor.consecutive < 1 && Vitkor.tmpDmg === +damage) {
+                Vitkor.consecutive++;
+                Vitkor.total += +damage;
+            } else if (Vitkor.consecutive === 1 && Vitkor.tmpDmg === +damage) {
+                Vitkor.consecutive = 0;
+                Vitkor.tmpDmg = +damage;
+                Vitkor.total += (damage * 2.75);
+            } else {
+                Vitkor.consecutive = 1;
+                Vitkor.tmpDmg = +damage;
+                Vitkor.total += +damage;
+            }
+        }
+    }
+
+    console.log(`Winner - ${Naskor.total < Vitkor.total ? "Vitkor" : "Naskor"}`);
+    console.log(`Damage - ${Naskor.total < Vitkor.total ? Vitkor.total * 60 : Naskor.total * 60}`);
+}
