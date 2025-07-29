7eaa059b78e2a9479929640c3037cee8e3d15ea9
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-07-29 15:44:39 +0300

    Add solution to task 2: Reveal Triangles

---
+++ 19. JavaScript Basics Exam - 27 July 2014/02. Reveal Triangles/revealTriangles.js
@@ -0,0 +1,22 @@
+function revealTriangles(params) {
+    let output = params.map(m => m = m.split(''));
+
+    for (let row = 1; row < params.length; row++) {
+        let maxCol = Math.min(params[row - 1].length - 2, params[row].length - 3);
+
+        for (let col = 0; col <= maxCol; col++) {
+            let a = params[row][col];
+            let b = params[row][col + 1];
+            let c = params[row][col + 2];
+            let d = params[row - 1][col + 1];
+            if (a == b && b == c & c == d) {
+                output[row][col] = '*';
+                output[row][col + 1] = '*';
+                output[row][col + 2] = '*';
+                output[row - 1][col + 1] = '*';
+            }
+        }
+    }
+
+    for (let row = 0; row < params.length; row++) console.log(output[row].join(''));
+}
