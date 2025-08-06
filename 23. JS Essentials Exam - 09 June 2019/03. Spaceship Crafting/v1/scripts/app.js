b9ad2aba7a8d0928e1260b9f1f48f419c6a25b73
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-08-06 13:01:45 +0300

    Add solution 88/100 to task 3: Spaceship Crafting

--- 23. JS Essentials Exam - 09 June 2019/03. Spaceship Crafting/v1/scripts/app.js
+++ 23. JS Essentials Exam - 09 June 2019/03. Spaceship Crafting/v1/scripts/app.js
@@ -1,3 +1,53 @@
 function spaceshipCrafting() {
-	//TODO...
+    let theUndefinedShip = 0, NullMaster = 0, JSONCrew = 0, FalseCrew = 0;
+
+    const lossesPercent = (100 - (document.getElementById("lossesPercent").value / 4)) / 100;
+    let titanium = Math.round(document.getElementById("titaniumCoreFound").value / 25 * lossesPercent);
+    let aluminium = Math.round(document.getElementById("aluminiumCoreFound").value / 50 * lossesPercent);
+    let magnesium = Math.round(document.getElementById("magnesiumCoreFound").value / 75 * lossesPercent);
+    let carbon = Math.round(document.getElementById("carbonCoreFound").value / 100 * lossesPercent);
+
+    while (true) {
+        if (7 <= titanium && 9 <= aluminium && 7 <= magnesium && 7 <= carbon) {
+            theUndefinedShip++;
+            titanium -= 7;
+            aluminium -= 9;
+            magnesium -= 7;
+            carbon -= 7;
+        }
+        if (5 <= titanium && 7 <= aluminium && 7 <= magnesium && 5 <= carbon) {
+            NullMaster++;
+            titanium -= 5;
+            aluminium -= 7;
+            magnesium -= 7;
+            carbon -= 5;
+        }
+        if (3 <= titanium && 5 <= aluminium && 5 <= magnesium && 2 <= carbon) {
+            JSONCrew++;
+            titanium -= 3;
+            aluminium -= 5;
+            magnesium -= 5;
+            carbon -= 2;
+        }
+        if (2 <= titanium && 2 <= aluminium && 3 <= magnesium && 1 <= carbon) {
+            FalseCrew++;
+            titanium -= 2;
+            aluminium -= 2;
+            magnesium -= 3;
+            carbon -= 1;
+        } else break;
+    }
+
+    let builtSpaceships = ``,
+        availableBars = `${titanium} titanium bars, ${aluminium} aluminum bars, ${magnesium} magnesium bars, ${carbon} carbon bars`;
+
+    if (0 < theUndefinedShip) builtSpaceships += `${theUndefinedShip} THE-UNDEFINED-SHIP`;
+    if (0 < NullMaster) builtSpaceships += `, ${NullMaster} NULL-MASTER`;
+    if (0 < JSONCrew) builtSpaceships += `, ${JSONCrew} JSON-CREW`;
+    if (0 < FalseCrew) builtSpaceships += `, ${FalseCrew} FALSE-FLEET`;
+
+    document.getElementById("availableBars").innerHTML = `<h1>Available Bars</h1>
+	<p>${availableBars}</p>`;
+    document.getElementById("builtSpaceships").innerHTML = `<h1>Built Spaceships</h1>
+	<p>${builtSpaceships}</p>`;
 }
